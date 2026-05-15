aecdd8e2f2c1e398aa4609a4e3bc92fce117bcf5
Diego Valadares <disvaladares@gmail.com>
2026-05-15 20:02:43 -0300

    Atualizando o README e a main.py para iniciar o projeto

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-db_host = os.getenv("DB_HOST")
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-api_key = os.getenv("API_KEY")
-
-print(f"Conectando em {db_host} com usuário {db_user}")
-print("Credenciais carregadas com sucesso!")
 
